f32a90a5f77545aa71cf8605b67df39c4ab76ba9
Binghi <binghi@Binghis-MBP.lan>
2025-08-28 03:44:47 -0400

    chore: vercel-ready (deps, sass, vite config, rewrites)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
 
 // https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/",                 // important for Vercel
+  build: {
+    outDir: "dist",          // Vercel will serve this
+    sourcemap: true
+  }
+});
+
